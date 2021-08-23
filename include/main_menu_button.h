0a56d39a1b34517089c07242403cb817696c6f2b
HvarZ <hvarzahar@gmail.com>
2021-08-23 22:36:15 +0300

    fixup! refactor and optimize main_menu_button.h: add check in animation functions on self-assignment

--- include/main_menu_button.h
+++ include/main_menu_button.h
@@ -19,28 +19,21 @@ private:
     const unsigned int increasedCharSize_ = 60;
     const unsigned int normalCharSize_ = 50;
 public:
+    // Constructors
     MainMenuButton() = default;
-
     [[maybe_unused]] MainMenuButton(const std::string& text, const sf::Vector2<float>& buttonPosition,
            const sf::Vector2<float>& buttonSize, const sf::Color& textColor = sf::Color::Green,
            unsigned int charSize = 50);
 
+    // Methods
     [[maybe_unused]] void SetFont(const sf::Font& font) noexcept;
-
     [[maybe_unused]] void SetColor(const sf::Color& color) noexcept;
-
     [[maybe_unused]] void SetPosition(const sf::Vector2<float>& newPosition) noexcept;
-
     [[maybe_unused]] void MagnifyingAnimation() noexcept;
-
     [[maybe_unused]] void ShrinkingAnimation() noexcept;
-
     [[maybe_unused]] void Draw(sf::RenderWindow& window) const noexcept;
-
     [[maybe_unused]] void SetSize(const sf::Vector2<float>& newSize) noexcept;
-
     [[maybe_unused]] auto IsOverMouse(sf::RenderWindow& window) const noexcept -> bool;
-
 };
 
 #endif //WHO_IS_BILLY_HERE_MAIN_MENU_BUTTON_H
